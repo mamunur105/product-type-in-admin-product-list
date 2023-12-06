f6524502f52a9b180a2979cebacc584f853f51a4
Mamunur Rashid <rmamunur105@gmail.com>
2023-12-06 22:38:27 +0600

    Version:           1.0.0

---
+++ product-type-in-product-table.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Show Product type in product list table
+ * Plugin URI:        https://wordpress.org/plugins/product-type-in-product-table
+ * Description:       Integrate custom post type with woocommerce. Sell Any Kind Of Custom Post
+ * Version:           1.0.0
+ * Author:            Tiny Solutions
+ * Author URI:        https://www.wptinysolutions.com/
+ * Tested up to:      6.4
+ * WC requires at least:3.2
+ * WC tested up to:     8.3.0
+ * Text Domain:       ptiplt
+ * Domain Path:       /languages
+ *
+ * @package TinySolutions\ptiplt
+ */
+
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'This script cannot be accessed directly.' );
+}
+
+function action_manage_product_custom_column( $column, $postid ) {
+	if ( $column == 'name' ) {
+		// Get product
+		$product = wc_get_product( $postid );
+		// Get type
+		$product_type = $product->get_type();
+		// Output
+		echo '&nbsp;<span> &ndash; ' .  ucfirst( $product_type ) . '</span>';
+	}
+}
+add_action( 'manage_product_posts_custom_column', 'action_manage_product_custom_column', 20, 2 );
