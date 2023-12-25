498098a62913d3bf73ba080f5ef7a60f37f6f46c
Mamunur Rashid <rmamunur105@gmail.com>
2023-12-25 15:50:32 +0600

    COmposer added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mamunurrashid/product-type-in-product-table',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '629c2f0c28c8459cab8f02c3764b03ae358902c0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mamunurrashid/product-type-in-product-table' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '629c2f0c28c8459cab8f02c3764b03ae358902c0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
