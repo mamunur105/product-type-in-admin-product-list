e77f4eb63db965b69386ced855c93e6d382b1541
Mamunur Rashid <rmamunur105@gmail.com>
2024-01-12 00:56:24 +0600

    Fixed

--- product-type-in-the-table.php
+++ product-type-in-the-table.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @wordpress-plugin
- * Plugin Name:       Show product type within the product table
- * Plugin URI:        https://wordpress.org/plugins/product-type-in-product-table
+ * Plugin Name:       Product type in the product table
+ * Plugin URI:        https://wordpress.org/plugins/product-type-in-the-table
  * Description:       The name of the product type is written after the name of the product in the admin area where you can see all the products.
  * Version:           0.0.1
  * Author:            Mamunur Rashid
